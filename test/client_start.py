0d8e06bb52fe642f4d1f972625d946fe121cb73f
Taxi_tabby <rkdmf0000@gmail.com>
2025-03-27 02:54:51 +0900

    Update README with additional notes on SQLAlchemy model creation; modify client_start.py to clarify URI duplication handling

--- test/client_start.py
+++ test/client_start.py
@@ -20,6 +20,7 @@ from CCRE_Auto_Internet_URI_Scrapper_with_Classification_AI.schema.implement.scr
 from CCRE_Auto_Internet_URI_Scrapper_with_Classification_AI.schema.implement.scrapper_root_access_rule import Scrapper_Root_Access_Rule
 
 
+# create rds db connection
 rds_connection = Connection_Info()
 rds_connection.db_type = DatabaseType.SQLITE3
 rds_connection.database = "test.db"
@@ -30,9 +31,15 @@ client_start(
     db_connection=rds_connection,
     roots=[
         Scrapper_Root('google', 'https://www.google.com', Scrapper_Root_Access_Rule(
-            skip_duplication_uri=True, # URI 중복을 건너뛰는지 여부
-            refresh_duplicate_uri=False, # URI 중복을 갱신하는지 여부
-            refresh_duplicate_uri_count=0 # URI 중복 갱신 횟수
+            
+            # URI 중복을 건너뛰는지 여부 (False 인 경우 특정 페이지를 무한하게 순환할 가능성이 있음.)
+            skip_duplication_uri=False, 
+            
+            # URI 중복을 갱신하는지 여부 (갱신은 해당 URI를 다시 분별하고 점수를 매기는 것을 의미합니다)
+            refresh_duplicate_uri=False, 
+            # URI 중복이 어느정도 누적되면 갱신할지 
+            refresh_duplicate_uri_count=0 
+            
             )),
     ]
 )
